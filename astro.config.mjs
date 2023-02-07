4a570ce2fa94e9136b5fccdbb10ce176c0865962
Samuele Mazzei <samumaz01@gmail.com>
2023-02-07 18:31:17 +0100

    Added UnoCSS and presets

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,13 @@
-import { defineConfig } from 'astro/config';
+import UnoCSS from "@unocss/astro";
+import { presetWind } from "@unocss/preset-wind";
+import { presetIcons } from "@unocss/preset-icons";
+import { presetTypography } from "@unocss/preset-typography";
 
 // https://astro.build/config
-export default defineConfig({});
+export default {
+  integrations: [
+    UnoCSS({
+      presets: [presetWind(), presetIcons(), presetTypography()],
+    }),
+  ],
+};
