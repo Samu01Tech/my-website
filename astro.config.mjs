4cc861b76261375fe9ba7d56ba626f239b379867
Samuele Mazzei <samumaz01@gmail.com>
2023-02-09 14:21:16 +0100

    removed unocss

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,2 @@
-import UnoCSS from "@unocss/astro";
-import { presetWind } from "@unocss/preset-wind";
-import { presetIcons } from "@unocss/preset-icons";
-import { presetTypography } from "@unocss/preset-typography";
-
 // https://astro.build/config
-export default {
-  integrations: [
-    UnoCSS({
-      presets: [presetWind(), presetIcons(), presetTypography()],
-    }),
-  ],
-};
+export default {};
