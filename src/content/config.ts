6148553e9afe95ae9fd79df3becd566878973aa5
Samuele Mazzei <samumaz01@gmail.com>
2023-02-09 19:00:48 +0100

    setup content folder

---
+++ src/content/config.ts
@@ -0,0 +1,35 @@
+import { z, defineCollection } from "astro:content";
+
+const projectsCollection = defineCollection({
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    image: z.object({
+      src: z.string(),
+      alt: z.string(),
+    }),
+  }),
+});
+
+const blogCollection = defineCollection({
+  schema: z.object({
+    isDraft: z.boolean(),
+    title: z.string(),
+    description: z.string(),
+    image: z.object({
+      src: z.string(),
+      alt: z.string(),
+    }),
+    author: z.string().default("Samuele M."),
+    language: z.enum(["it", "en"]),
+    publishDate: z.string().transform((str) => new Date(str)),
+    editDate: z.date(),
+    authorContact: z.string().email(),
+    canonicalURL: z.string().url(),
+  }),
+});
+
+export const collections = {
+  projects: projectsCollection,
+  blog: blogCollection,
+};
