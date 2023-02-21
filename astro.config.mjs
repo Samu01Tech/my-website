d64455a7a39bfe4dbf5be9221ed4b32c6d503201
Samuele Mazzei <samumaz01@gmail.com>
2023-02-21 23:38:47 +0100

    fixed typo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,7 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import Icons from "unplugin-icons/vite";
-import { defineConfig } from 'astro/config'
-
-export default defineConfig({
-  site: 'https://astronaut.github.io',
-  base: '/my-repo',
-})
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
