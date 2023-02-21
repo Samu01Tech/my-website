224c52864583f48887bcb201236e4c0fcbfce478
Samuele Mazzei <samumaz01@gmail.com>
2023-02-21 23:50:44 +0100

    removed double defineConfig

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import Icons from "unplugin-icons/vite";
-import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
